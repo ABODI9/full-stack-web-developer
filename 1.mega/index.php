333147088ad2875147fdc1ff8e9bf76b07fa4d12
Abdallah Hamada <120796923+ABODI9@users.noreply.github.com>
2025-03-09 14:56:42 +0300

    Add files via upload

---
+++ 1.mega/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="action.php" method="post">
+        <label for="username">username:</label>
+        <input type="text" name="username" id="username" placeholder="Enter username ...">
+        <br>
+        <label for="email">Email :</label>
+        <input type="email" name="email" id="email" placeholder="Enter email">
+        <br>
+        <label for="password">Password:</label>
+        <input type="password" name="password" id="password" placeholder="Enter password...">
+        <br>
+        <label for="phone">phone</label>
+        <input type="text" id="phone" name="phone" placeholder="Enter phone ....">
+        <br>
+        <button type="submit">Save</button>
+
+    </form>
+</body>
+</html>
